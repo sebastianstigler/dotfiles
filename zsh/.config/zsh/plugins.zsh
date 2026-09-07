f71895bd8001ffb84b87f6d2c9251e100ebcbea6
Sebastian Stigler <sebastian.stigler@hs-aalen.de>
2026-09-07 13:51:31 +0200

    deactivate vi mode in zsh as is clashes with fzf

--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -24,6 +24,6 @@ zplugin-update() {
 }
 
 _zplugin_load zsh-users zsh-autosuggestions
-_zplugin_load zsh-users zsh-history-substring-search
-_zplugin_load jeffreytse zsh-vi-mode
+#_zplugin_load zsh-users zsh-history-substring-search
+#_zplugin_load jeffreytse zsh-vi-mode
 _zplugin_load zdharma-continuum fast-syntax-highlighting
