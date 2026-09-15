d5df1aeaa657f1bebfc0375267ca05005e0f0030
Sebastian Stigler <sebastian.stigler@hs-aalen.de>
2026-09-15 16:52:36 +0200

    add p10k like transient prompt to starship

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -6,3 +6,23 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 FUNCNEST=100
 
 eval "$(starship init zsh)"
+
+# Starship Transient Prompt Workaround for Zsh
+starship_zle-line-init() {
+    if [[ "$CONTEXT" == start ]]; then
+        # Re-render the full Starship prompt for active input
+        PROMPT=$(starship prompt)
+        zle reset-prompt
+    fi
+}
+
+starship_zle-line-finish() {
+    # Replace the prompt with just the character when executing
+    PROMPT='%F{blue}❯%f '
+
+    zle reset-prompt
+}
+
+# Register the hooks into Zsh line editor
+zle -N zle-line-init starship_zle-line-init
+zle -N zle-line-finish starship_zle-line-finish
