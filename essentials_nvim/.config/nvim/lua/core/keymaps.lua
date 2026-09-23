c3db01fe16c9a1932d4e68b78c8734830360d3d0
Sebastian Stigler <sebastian.stigler@hs-aalen.de>
2026-09-23 15:48:32 +0200

    ust <C-s> also in insert mode to save the file

--- essentials_nvim/.config/nvim/lua/core/keymaps.lua
+++ essentials_nvim/.config/nvim/lua/core/keymaps.lua
@@ -10,6 +10,7 @@ local opts = { noremap = true, silent = true }
 
 -- save file
 vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+vim.keymap.set('i', '<C-s>', '<Esc><cmd> w <CR>', opts)
 
 -- save file without auto-formatting
 vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
